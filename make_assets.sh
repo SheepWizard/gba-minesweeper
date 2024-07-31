04889fe14453211e3a853877df488a5034f545ee
sheep_wizard <you@example.com>
2024-07-31 21:24:10 +0100

    update assets

--- make_assets.sh
+++ make_assets.sh
@@ -5,7 +5,7 @@ rm -rf $OUTPATH
 mkdir -p $OUTPATH
 cd $OUTPATH
 
-grit $ASSETS/cell_hidden.bmp -gb -gB16
+grit $ASSETS/cell_hidden.png -gb -gB16
 grit $ASSETS/cell_1.png -gb -gB16
 grit $ASSETS/cell_2.png -gb -gB16
 grit $ASSETS/cell_3.png -gb -gB16
@@ -14,12 +14,12 @@ grit $ASSETS/cell_5.png -gb -gB16
 grit $ASSETS/cell_6.png -gb -gB16
 grit $ASSETS/cell_7.png -gb -gB16
 grit $ASSETS/cell_8.png -gb -gB16
-grit $ASSETS/cell_open.bmp -gb -gB16
-grit $ASSETS/cell_hovered.bmp -gb -gB16
-grit $ASSETS/cell_mine.bmp -gb -gB16
-grit $ASSETS/cell_flag.bmp -gb -gB16
-grit $ASSETS/cell_minehit.bmp -gb -gB16
-grit $ASSETS/cell_wrongflag.bmp -gb -gB16
+grit $ASSETS/cell_open.png -gb -gB16
+grit $ASSETS/cell_hovered.png -gb -gB16
+grit $ASSETS/cell_mine.png -gb -gB16
+grit $ASSETS/cell_flag.png -gb -gB16
+grit $ASSETS/cell_minehit.png -gb -gB16
+grit $ASSETS/cell_wrongflag.png -gb -gB16
 grit $ASSETS/number_0.bmp -gb -gB16
 grit $ASSETS/number_1.bmp -gb -gB16
 grit $ASSETS/number_2.bmp -gb -gB16
